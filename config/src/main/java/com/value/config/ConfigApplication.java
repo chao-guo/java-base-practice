a179407c2d74b4899d129a46c550efb2be23473d
chao.guo <wsgcwaxx@126.com>
2022-02-19 15:27:44 +0800

    init

--- config/src/main/java/com/value/config/ConfigApplication.java
+++ config/src/main/java/com/value/config/ConfigApplication.java
@@ -1,5 +1,7 @@
 package com.value.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConfigApplication {
 
     public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(ConfigApplication.class);
         SpringApplication.run(ConfigApplication.class, args);
+        logger.info("Deployment done");
     }
 
 }
