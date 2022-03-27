9f7f847e3e43987dfaa9b56362cc25fd68927731
chao.guo <wsgcwaxx@126.com>
2022-03-27 12:15:56 +0800

    decorator command

---
+++ designpattern/src/main/java/com/practice/designpattern/command/Command.java
@@ -0,0 +1,10 @@
+package com.practice.designpattern.command;
+
+/**
+ * @author chao.guo
+ * @Description TODO
+ * @create 2020年07月05日 21:30:00
+ */
+public interface Command {
+    public abstract void execute();
+}
